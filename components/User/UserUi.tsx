6868bd2d8dad249981b21e5f14dd82f0176960f9
Rafael Augusto <movcode@gmail.com>
2020-09-13 18:21:16 -0300

    feat: add title in UserUi

--- components/User/UserUi.tsx
+++ components/User/UserUi.tsx
@@ -1,13 +1,15 @@
 import { Grid } from '@material-ui/core'
-import { CustomContainer } from '../ShareComponents'
+import { CustomContainer, Title } from '../ShareComponents'
 import Form from './UserForm'
 import List from './UserList'
 const UserUi = () => {
   const add = (values) => console.log(values)
   return (
     <CustomContainer>
-
       <Grid container>
+        <Grid container justify='center' alignItems='center'>
+          <Title>Gerenciador de Usuário</Title>
+        </Grid>
 
         <Grid item xs={12} md={6}>
           <Form handleSubmit={add} />
@@ -18,7 +20,6 @@ const UserUi = () => {
         </Grid>
 
       </Grid>
-
     </CustomContainer>
   )
 }
