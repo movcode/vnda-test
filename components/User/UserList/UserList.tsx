1f93c959dbf597578c27f0f1b807cc3bf82d2d8b
Rafael Augusto <movcode@gmail.com>
2020-09-13 16:38:24 -0300

    feat: create list  user

---
+++ components/User/UserList/UserList.tsx
@@ -0,0 +1,8 @@
+
+const UserList = () => {
+  return (
+    <div>User List</div>
+  )
+}
+
+export default UserList
--- components/User/UserUi.tsx
+++ components/User/UserUi.tsx
@@ -1,14 +1,23 @@
 import { Grid } from '@material-ui/core'
 import { CustomContainer } from '../ShareComponents'
 import Form from './UserForm'
+import List from './UserList'
 const UserUi = () => {
   const add = (values) => console.log(values)
   return (
     <CustomContainer>
       <>
-        <Grid>
-          <Form handleSubmit={add} />
+
+        <Grid container >
+          <Grid item xs={12} md={6}>
+            <Form handleSubmit={add} />
+          </Grid>
+
+          <Grid item xs={12} md={6}>
+            <List />
+          </Grid>
         </Grid>
+
       </>
     </CustomContainer>
   )
