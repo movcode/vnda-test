b61f0c1b5e97040c581768015dd2ee0de2b265df
Rafael Augusto <movcode@gmail.com>
2020-09-14 20:40:04 -0300

    create server express and middleware cors

---
+++ backend/config/server.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+
+const cors = require('../middlewares/Cors')
+require('./mongodb')
+
+const app = express()
+app.use(express.urlencoded({ extended: true }))
+app.use(bodyParser.json())
+app.use(cors)
+
+const port = process.env.PORT || 3000
+try {
+  app.listen(port)
+} catch (err) {
+  console.log(err)
+  process.exit(1)
+}
+module.exports = app
