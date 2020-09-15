da79cc37abfeecf1ff4cd2221d04dac9d353bb68
Rafael Augusto <movcode@gmail.com>
2020-09-14 22:58:33 -0300

    feat: add in user repository update http clent

--- nextjs/components/User/UserRepository.ts
+++ nextjs/components/User/UserRepository.ts
@@ -1,10 +1,11 @@
 import { HttpClient } from '../../services'
 import { UserType } from './Types'
 
-const path = 'users'
+const path = 'user'
 
 export default ({
   list: async () => await HttpClient().get(path, true),
   post: async (user:UserType) => await HttpClient().post(path, user),
-  remove: async (id:string) => await HttpClient().remove(`${path}/${id}`, true)
+  remove: async (id:string) => await HttpClient().remove(`${path}/${id}`, true),
+  update: async (id:string) => await HttpClient().update(`${path}/${id}`, true)
 })
