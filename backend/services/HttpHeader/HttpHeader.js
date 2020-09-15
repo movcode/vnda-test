af6b7fdd2651f1505e2fc489866d03071b4ff684
Rafael Augusto <movcode@gmail.com>
2020-09-14 23:03:23 -0300

    feat: add list users

--- backend/services/HttpHeader/HttpHeader.js
+++ backend/services/HttpHeader/HttpHeader.js
@@ -2,19 +2,15 @@ require('dotenv').config()
 
 const headerWithAuthorization = ({
   'Content-Type': 'application/json',
-  withCredentials: false,
-  'Access-Control-Allow-Origin': '*',
-  'Access-Control-Allow-Methods': 'GET,PUT,POST,DELETE,PATCH,OPTIONS',
   Authorization: process.env.TOKEN
 })
 
 const header = ({
   'Content-Type': 'application/json',
-  withCredentials: false,
   'Access-Control-Allow-Origin': '*',
   'Access-Control-Allow-Methods': 'GET,PUT,POST,DELETE,PATCH,OPTIONS'
 })
 
 const HttpHeader = (auth) => ({ headers: auth ? headerWithAuthorization : header })
 
-export default HttpHeader
+module.exports = HttpHeader
