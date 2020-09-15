8643769e3636f3e16b36cd1c8e10aa14ff7abd19
Rafael Augusto <movcode@gmail.com>
2020-09-14 22:59:52 -0300

    test: remove test api store

--- backend/api/users/user.test.js
+++ backend/api/users/user.test.js
@@ -1,4 +1,15 @@
+const request = require('supertest')
+const { server, app } = require('../../config/server')
 
 describe('user integration test ', () => {
+  afterAll(done => {
+    server.close()
+    done()
+  })
 
+  it('should return error 404 if not send params', async () => {
+    await request(app)
+      .post('/api/user')
+      .expect(404)
+  })
 })
